08c6744fe634d701ab3d9072d46b6e7c07fc3285
mtshun <mt.s.pass13@icloud.com>
2018-06-19 18:14:30 +0900

    Add webpackerの初期設定をする.CoffeeScriptの自動生成をオフにする.

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module ReactTestApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # CoffeeScriptの自動生成をオフ
+    config.generators do |g|
+      # ...generatorの設定
+      g.javascripts     false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
