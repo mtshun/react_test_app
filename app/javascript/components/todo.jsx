95878c374a5a26a62178957fcec44355a5ecde95
mtshun <mt.s.pass13@icloud.com>
2018-07-28 16:10:57 +0900

    Add React Todo管理機能実装

--- app/javascript/components/todo.jsx
+++ app/javascript/components/todo.jsx
@@ -1,46 +1,184 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
+const todos = [];
 
-const todos = [
-  {id: 0, title: 'Task 0', isDone: false},
-  {id: 1, title: 'Task 1', isDone: false},
-  {id: 2, title: 'Task 2', isDone: true}
-]
+function TodoHeader(props) {
+  const remaining = props.todos.filter(todo => {
+    return !todo.isDone;
+  });
+
+  return (
+    <h1>
+      <button onClick={props.purge}>完了済み削除</button>
+      My Todos
+      <span>
+        ({remaining.length}/{props.todos.length})
+      </span>
+    </h1>
+  );
+}
+
+function TodoItem(props) {
+  return (
+    <li>
+      <label>
+        <input
+          type="checkbox"
+          checked={props.todo.isDone}
+          onChange={() => props.checkTodo(props.todo)}
+        />
+        <span className={props.todo.isDone ? "done" : ""}>
+          {props.todo.title}
+        </span>
+      </label>
+      <span className="cmd" onClick={() => props.deleteTodo(props.todo)}>
+        [x]
+      </span>
+    </li>
+  );
+}
 
 function TodoList(props) {
-
   const todos = props.todos.map(todo => {
     return (
-      <li key={todo.id}>{todo.title}</li>
-    )
-  })
+      <TodoItem
+        key={todo.id}
+        todo={todo}
+        checkTodo={props.checkTodo}
+        deleteTodo={props.deleteTodo}
+      />
+    );
+  });
+  return <ul>{props.todos.length ? todos : <p>何もないよーー！！</p>}</ul>;
+}
+
+function TodoForm(props) {
   return (
-    <ul>
-      {todos}
-    </ul>
-  )
+    <form onSubmit={props.addTodo}>
+      <input type="text" value={props.item} onChange={props.updateItem} />
+      <input type="submit" value="追加" />
+    </form>
+  );
+}
+
+function getUniqueId() {
+  return new Date().getTime().toString(36) + "-" + Math.random().toString(36);
 }
 
 class App extends React.Component {
-
   constructor() {
-    super()
+    super();
     this.state = {
-      todos: todos
-    }
+      todos: todos,
+      item: ""
+    };
+    this.checkTodo = this.checkTodo.bind(this);
+    this.deleteTodo = this.deleteTodo.bind(this);
+    this.updateItem = this.updateItem.bind(this);
+    this.addTodo = this.addTodo.bind(this);
+    this.purge = this.purge.bind(this);
   }
 
+  purge() {
+    if (!confirm("本当に削除しますか？")) {
+      return;
+    }
+    const todos = this.state.todos.filter(todo => {
+      return !todo.isDone;
+    });
+    this.setState({
+      todos: todos
+    });
+  }
+
+  checkTodo(todo) {
+    const todos = this.state.todos.map(todo => {
+      return {
+        id: todo.id,
+        title: todo.title,
+        isDone: todo.isDone
+      };
+    });
+    const pos = this.state.todos
+      .map(todo => {
+        return todo.id;
+      })
+      .indexOf(todo.id);
+
+    todos[pos].isDone = !todos[pos].isDone;
+    this.setState({
+      todos: todos
+    });
+  }
+
+  deleteTodo(todo) {
+    if (!confirm("本当に消す？")) {
+      return;
+    }
+    const todos = this.state.todos.slice();
+    const pos = this.state.todos.indexOf(todo);
+    todos.splice(pos, 1);
+    this.setState({
+      todos: todos
+    });
+  }
+
+  updateItem(e) {
+    this.setState({
+      item: e.target.value
+    });
+  }
+
+  addTodo(e) {
+    e.preventDefault();
+    if (this.state.item.trim() === "") {
+      return alert("文字を入れてね！");
+    }
+    const item = {
+      id: getUniqueId(),
+      title: this.state.item,
+      isDone: false
+    };
+
+    const todos = this.state.todos.slice();
+    todos.push(item);
+    this.setState({
+      todos: todos,
+      item: ""
+    });
+  }
+
+  // データ永続化
+  // 保存
+  componentDidUpdate() {
+    localStorage.setItem("todos", JSON.stringify(this.state.todos));
+  }
+  // データ読み込み
+  componentDidMount() {
+    this.setState({
+      todos: JSON.parse(localStorage.getItem("todos")) || []
+    });
+  }
 
   render() {
     return (
-      <div className='container'>
-        <h1>My todos</h1>
-        <TodoList todos={this.state.todos}/>
+      <div className="container">
+        <TodoHeader todos={this.state.todos} purge={this.purge} />
+        <TodoList
+          todos={this.state.todos}
+          checkTodo={this.checkTodo}
+          deleteTodo={this.deleteTodo}
+        />
+        <TodoForm
+          item={this.state.item}
+          updateItem={this.updateItem}
+          addTodo={this.addTodo}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
